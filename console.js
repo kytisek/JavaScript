feab9f40719ef70979be700099ef0b36286d6fc3
kytisek <kytissq@gmail.com>
2023-01-02 15:28:52 +0100

    js

--- console.js
+++ console.js
@@ -15,3 +15,29 @@ for (let i = 0; i < 100000; i++) {
     ;
 }
 console.timeEnd( "test 1"); //konczy test
+function test() {
+    let i = 0;
+   //debugger;
+    //instrukcja debugger przerywam działanie skryptu w tym miejscu,
+    // dzieki czemu moge spokojnie go badac w zakładce Source.
+    // Dodatkowo w konsoli mam dostęp do zmiennych z danego scope
+    // ta instrukcja przerywa natychmiast działanie kodu
+}
+//ZMIENNE
+var text ="ala";
+text = "Janek ma żyrafę"; //wszystko ok,  bo var to zmienna
+console.log({text});
+
+let a = 0;
+a = 10; // wszystko ok, bo let
+const b = 0;
+//b = 10; // błąd - do stałej nie możemy przypisać nowej wartości
+
+const name="Ala";
+//name = "Monika"; błąd
+
+const tab = [1,2,3]; //tablica to też obiekt
+tab[3] = 4;
+tab.push(5, 6);
+console.log(tab); //[1, 2, 3, 4, 5, 6]
+//tab = [1, 2, 3, 4, 5, 6]; //błąd - podstawiłem zupełnie nową tablicę
