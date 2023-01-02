807e99a89eb0b5ddbe55c785c1616660cfff868a
kytisek <kytissq@gmail.com>
2023-01-02 14:44:44 +0100

    js

---
+++ console.js
@@ -0,0 +1,8 @@
+console.log("Witaj Świecie");
+console.log(window);
+console.log("Witaj", window, 100);
+console.table([1, 2, 3, 4, 5]);
+console.group("Nazwa grupy");
+console.log("Ala ma kota");
+console.log("Kot ma Alę");
+console.groupEnd();
