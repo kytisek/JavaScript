90c38173a62a639d90c51cec156d418a86c5d4cb
kytisek <kytissq@gmail.com>
2023-01-09 15:33:57 +0100

    zad  1

--- zadanie1.js
+++ zadanie1.js
@@ -3,8 +3,14 @@ const pluszek = document.querySelector('header');
 pluszek.style.backgroundColor='yellow';
 console.log(pluszek);
 //2
-const drugie = document.querySelector('header h1')
-console.log(drugie)
+const drugie = document.querySelector('header h1');
+console.log(drugie);
+drugie.style.fontFamily = 'Arial Black';
+drugie.innerHTML = 'Preambuła';
+//3
+const three = document.getElementsByTagName("section");
+three.
+
 
 
 
