84d47bd63d734f64ea8443335425196300991d8d
kytisek <kytissq@gmail.com>
2023-01-13 09:51:48 +0100

    zad1

--- zadanie1.js
+++ zadanie1.js
@@ -8,11 +8,14 @@ console.log(drugie);
 drugie.style.fontFamily = 'Arial Black';
 drugie.innerHTML = 'Preambuła';
 //3
-const three = document.getElementsByTagName("section");
-three.
-
-
-
-
-
-
+const sections = document.querySelectorAll("section");
+console.log(sections);
+for (const section of sections){
+    section.style.border = "2px solid brown";
+    console.log(section);
+}
+//4
+const czter = document.querySelector("section.main_column h1");
+console.log(czter);
+czter.innerHTML = 'Specjalność szefa';
+//5
