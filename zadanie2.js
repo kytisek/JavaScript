e5ae1b2103043d56403f66769d9ab4730c69417d
kytisek <kytissq@gmail.com>
2023-02-17 10:08:24 +0100

    z2

---
+++ zadanie2.js
@@ -0,0 +1,15 @@
+//1
+const uno = document.getElementById("b1")
+uno.addEventListener("click", function(){
+    document.getElementById('z1').innerHTML = "<i>Swietnie!</i>";
+});
+//2
+const two = document.getElementById("b2");
+two.addEventListener("click", function(){
+    document.getElementById('z2').innerText = "<i>Swietnie!</i>";
+});
+//3
+const tre = document.getElementById("z3");
+tre.addEventListener("click", function(){
+    document.getElementById('');
+
