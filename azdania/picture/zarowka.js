8e48f976067262fe7e288664327020ecef761ebb
kytisek <kytissq@gmail.com>
2023-03-24 09:55:55 +0100

    zar

---
+++ azdania/picture/zarowka.js
@@ -0,0 +1,10 @@
+document.querySelector("#b2").addEventListener('click', function (){
+    document.getElementById("img1").src="wylacz.png"
+
+
+})
+document.querySelector("#b1").addEventListener('click', function (){
+    document.getElementById("img1").src="wlacz.png"
+
+
+})
