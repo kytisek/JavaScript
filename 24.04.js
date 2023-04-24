76c90ecbbc99e86c3b08ba873156fcecd1dc1e29
kytisek <kytissq@gmail.com>
2023-04-24 15:32:46 +0200

    4s

---
+++ 24.04.js
@@ -0,0 +1,7 @@
+const misiaczek = document.getElementById("pp");
+const misi = document.getElementById("hh");
+let a = Math.abs(-6.5)
+let b = Math.abs(4.3)
+console.log(misiaczek);
+misiaczek.innerHTML = a;
+
