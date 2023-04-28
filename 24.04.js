3a8e41dd3d90094bd53a03a103b2e87f34af367c
kytisek <kytissq@gmail.com>
2023-04-28 10:15:39 +0200

    zar

--- 24.04.js
+++ 24.04.js
@@ -1,7 +1,16 @@
-const misiaczek = document.getElementById("pp");
-const misi = document.getElementById("hh");
-let a = Math.abs(-6.5)
-let b = Math.abs(4.3)
-console.log(misiaczek);
-misiaczek.innerHTML = a;
-
+const misiaczek = prompt('Podaj liczbe liczbę')
+const spannumbers = document.getElementsByClassName("jeden");
+document.getElementsByClassName('mnms' + misiaczek)
+for (const num of spannumbers){
+    num.innertext=misiaczek;
+}
+for (const num of spannumbers){
+    document.getElementsByClassName('sp1').innerText = Math.abs(spannumbers)
+    document.getElementsByClassName('sp2').innerText = Math.ceil(spannumbers)
+    document.getElementsByClassName('sp3').innerText = Math.floor(spannumbers)
+    document.getElementsByClassName('sp4').innerText = Math.max(spannumbers)
+    document.getElementsByClassName('sp5').innerText = Math.min(spannumbers)
+    document.getElementsByClassName('sp6').innerText = Math.pow(spannumbers)
+    document.getElementsByClassName('sp7').innerText = Math.round(spannumbers)
+    document.getElementsByClassName('sp8').innerText = Math.sqrt(spannumbers)
+}
