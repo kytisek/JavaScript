9167e003153a5bd710730e6ab0b9b5b38f6bf1c1
kytisek <kytissq@gmail.com>
2023-06-05 15:30:48 +0200

    333s

---
+++ 05.06.23 slice.js
@@ -0,0 +1,23 @@
+
+//dsa
+var pierwsze = document.getElementsByClassName('resq')[0].innerText;
+var one = document.getElementById('zaa');
+one.addEventListener('click', function (){
+    var wwa = pierwsze.replaceAll("Microsoft","JanuszPol");
+    document.getElementById('mmm').innerHTML=wwa;
+});
+
+
+//para
+var powat = document.getElementsByClassName('resta')[0].innerText;
+var bow = document.getElementById('zaw');
+bow.addEventListener('click', function (){
+    var mma = powat.toUpperCase();
+    document.getElementById('nnn').innerHTML=mma;
+})
+
+var kow = document.getElementById('zaz');
+kow.addEventListener('click', function (){
+    var gga = powat.toUpperCase();
+    document.getElementById('nnn').innerHTML=gga;
+})
